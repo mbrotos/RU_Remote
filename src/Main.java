77d2a71ab59cd6f11b317ce357528a79254d442a
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2019-01-21 21:36:23 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Adam Sorrenti
+ * date: 2019-01-21
+ * description: Ryerson University Remote SSH Access! RU Remote????
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
