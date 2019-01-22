3b1797832053710d43fc55c0642ab7d71b74d2d5
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2019-01-21 22:48:24 -0500

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.net.URL;
+
 /**
  * @author Adam Sorrenti
  * date: 2019-01-21
@@ -7,6 +10,22 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        try {
+            URL path;
+            File file;
+            Runtime rf = Runtime.getRuntime();
+            Process pf;
+
+            path = Main.class.getClassLoader().getResource("resources/sshfs-win-2.7.17334-x64.msi");
+            file = new File(path.toURI());
+            pf = rf.exec("");
+            pf.waitFor();
+
+        } catch(Exception e) {
+            System.out.println(e.toString()); // not necessary
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/resources/RestoreConnection.ps1
@@ -0,0 +1,19 @@
+﻿# Creates a new registry key or updates the existing RestoreConnection key in order to stop mapped network drives from auto connection on start up.
+# This script makes persistent network drives only attempt connection when opened in explorer.
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-windowstyle hidden -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+#Now running with as admin
+
+$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\NetworkProvider"
+
+$Name = "RestoreConnection"
+
+$value = "0"
+
+IF(!(Test-Path $registryPath))#If the regitry doesn't already exist we create it
+{
+    New-Item -Path $registryPath -Force | Out-Null
+}
+
+New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null #set the value to 0
